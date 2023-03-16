22149214c8ce0e7cc519fb57a099eeebf7960005
lpsosa <lilian.perez@cimat.mx>
2023-03-16 09:55:31 -0600

    formato

--- Data_run.R
+++ Data_run.R
@@ -1,4 +1,5 @@
 
+
 source("Covariates_time.R")
 library(survival)
 
@@ -7,10 +8,10 @@ sepkoski00 = readRDS("sepkoski00.rds")
 
 
 #   ____________________________________________________________________________
-#   Create new database with time-dependent covariates                       ####
+#   Create new database with time-dependent covariates                      ####
 
 
-new_database = function(sepkoski00){
+new_database = function(sepkoski00) {
   x = sepkoski00
   x = subset(x, x$grupoid < 8 & x$longevidad >= 1)
   x$longevidad = as.integer(x$longevidad)
@@ -23,7 +24,7 @@ new_database = function(sepkoski00){
   t_dep[1:dim(t_dep)[1], 4] = rep(0, dim(t_dep)[1])
   ii = length(x$longevidad) - 1
   ini = 1
-  for(i in 1:ii){
+  for (i in 1:ii) {
     fin = ini + x$longevidad[i]
     lon = x$longevidad[i] + 1
     
@@ -31,28 +32,35 @@ new_database = function(sepkoski00){
     t_dep[ini:fin, 1] = rep(i, lon)
     
     ##genera
-    t_dep[ini:fin, 10] = rep(x$grupoid[i],lon)
+    t_dep[ini:fin, 10] = rep(x$grupoid[i], lon)
     
     ##event
-    if(x$observed[i] == TRUE) {t_dep[fin-1,4] = 1}
+    if (x$observed[i] == TRUE) {
+      t_dep[fin - 1, 4] = 1
+    }
     
     ##First appearance to later increase the longevity and see the covariates.
-    t_dep[ini:fin,8]=rep(x$FA[i],lon)  
+    t_dep[ini:fin, 8] = rep(x$FA[i], lon)
     
     ##Update
-    ini=fin
+    ini = fin
   }
-  t_dep[ini:dim(t_dep)[1], 10] = rep(x$grupoid[length(x$longevidad)], x$longevidad[length(x$longevidad)])
-  t_dep[ini:dim(t_dep)[1], 8] = rep(x$FA[length(x$longevidad)], x$longevidad[length(x$longevidad)])
-  t_dep[ini:dim(t_dep)[1], 1] = rep(length(x$longevidad), x$longevidad[length(x$longevidad)])
-  if(x$observed[length(x$longevidad)] == TRUE) {t_dep[dim(t_dep)[1], 4] = 1}
+  t_dep[ini:dim(t_dep)[1], 10] = rep(x$grupoid[length(x$longevidad)],
+                                     x$longevidad[length(x$longevidad)])
+  t_dep[ini:dim(t_dep)[1], 8] = rep(x$FA[length(x$longevidad)],
+                                    x$longevidad[length(x$longevidad)])
+  t_dep[ini:dim(t_dep)[1], 1] = rep(length(x$longevidad),
+                                    x$longevidad[length(x$longevidad)])
+  if (x$observed[length(x$longevidad)] == TRUE) {
+    t_dep[dim(t_dep)[1], 4] = 1
+  }
   
   ##intervals for each genera
   time0 = c()
   time1 = c()
   ind1 = 1
   iii = ii + 1
-  for(j in 1:iii){
+  for (j in 1:iii) {
     ind = ind1 + x$longevidad[j]
     long = x$longevidad[j]
     long1 = x$longevidad[j] + 1
@@ -72,49 +80,125 @@ new_database = function(sepkoski00){
   t_dep[, 11] = SST(t_dep[, 8])
   t_dep[, 12] = CO2(t_dep[, 8])
   t_dep[, 13] = SSTdiff(t_dep[, 8])
-  t_dep=data.frame(t_dep)
-  colnames(t_dep)=c("id","t_ini","t_fin","event","Sea_level","sinusoid62","sinusoid140","T","fitness","g_id","sst","CO2","SSTdiff")
+  t_dep = data.frame(t_dep)
+  colnames(t_dep) = c(
+    "id",
+    "t_ini",
+    "t_fin",
+    "event",
+    "Sea_level",
+    "sinusoid62",
+    "sinusoid140",
+    "T",
+    "fitness",
+    "g_id",
+    "sst",
+    "CO2",
+    "SSTdiff"
+  )
   return(t_dep)
 }
 
 
 #   ____________________________________________________________________________
-#   Estimates, standard errors and hazard multipliers per unit covariate for####
+#   Estimates, standard errors and hazard multipliers per unit covariate    ####
 
-simple_cox = function(t_dep){
-  return(coxph(Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level + t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness`+ t_dep$SSTdiff +relevel(as.factor(t_dep$g_id),ref = "7"), cluster = t_dep$id))
-  }
+                                                               
+##  ............................................................................
+##  explicación                                                             ####
 
 
-cox_complex = function(t_dep, model_cuts){
-  cut_Cox_mar = survSplit(Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level + t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness`+t_dep$SSTdiff + relevel(as.factor(t_dep$g_id),ref = "7"), data=data.frame(t_dep), cut=model_cuts[[1]],episode= "tgroup", id="id")
-  cut_Cox_sin_2 = survSplit(Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level + t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness`+t_dep$SSTdiff + relevel(as.factor(t_dep$g_id),ref = "7"), data=data.frame(t_dep), cut=model_cuts[[3]],episode= "tgroup9", id="id")
-  cut_Cox_evo = survSplit(Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level + t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness` +t_dep$SSTdiff+ relevel(as.factor(t_dep$g_id),ref = "7"), data=data.frame(t_dep), cut=model_cuts[[4]],episode= "tgroup2", id="id")
-  cut_Cox_grupos = survSplit(Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level + t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness`+t_dep$SSTdiff + relevel(as.factor(t_dep$g_id),ref = "7"), data=data.frame(t_dep), cut=model_cuts[[6]],episode= "tgroup11", id="id")
-  cut_Cox_f=cbind(cut_Cox_mar,cut_Cox_sin_2[,11],cut_Cox_evo[,11], cut_Cox_grupos[,11])
-  cox=coxph(Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level:strata(tgroup) + t_dep$sinusoid62  + t_dep$sinusoid140:strata(cut_Cox_sin_2[,11])+ t_dep$`fitness`:strata(cut_Cox_evo[,11]) +t_dep$SSTdiff + as.factor(t_dep$g_id):strata(cut_Cox_grupos[,11]) , data=cut_Cox_f)
-  return(list(cox,model_cuts,cox$coefficients))
+simple_cox = function(t_dep) {
+  return(
+    coxph(
+      Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level +
+        t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness` + t_dep$SSTdiff +
+        relevel(as.factor(t_dep$g_id), ref = "7"),
+      cluster = t_dep$id
+    )
+  )
 }
 
 
-list_of_coefficients = function(cox,nomb_coef){
+cox_complex = function(t_dep, model_cuts) {
+  cut_Cox_mar = survSplit(
+    Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level +
+      t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness` +
+      t_dep$SSTdiff + relevel(as.factor(t_dep$g_id), ref = "7"),
+    data = data.frame(t_dep),
+    cut = model_cuts[[1]],
+    episode = "tgroup",
+    id = "id"
+  )
+  cut_Cox_sin_2 = survSplit(
+    Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level +
+      t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness` +
+      t_dep$SSTdiff + relevel(as.factor(t_dep$g_id), ref = "7"),
+    data = data.frame(t_dep),
+    cut = model_cuts[[3]],
+    episode = "tgroup9",
+    id = "id"
+  )
+  cut_Cox_evo = survSplit(
+    Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level +
+      t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness` +
+      t_dep$SSTdiff + relevel(as.factor(t_dep$g_id), ref = "7"),
+    data = data.frame(t_dep),
+    cut = model_cuts[[4]],
+    episode = "tgroup2",
+    id = "id"
+  )
+  cut_Cox_grupos = survSplit(
+    Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~ t_dep$Sea_level + 
+      t_dep$sinusoid62 + t_dep$sinusoid140 + t_dep$`fitness` +
+      t_dep$SSTdiff + relevel(as.factor(t_dep$g_id), ref = "7"),
+    data = data.frame(t_dep),
+    cut = model_cuts[[6]],
+    episode = "tgroup11",
+    id = "id"
+  )
+  cut_Cox_f = cbind(cut_Cox_mar, cut_Cox_sin_2[, 11], cut_Cox_evo[, 11],
+                    cut_Cox_grupos[, 11])
+  cox = coxph(
+    Surv(t_dep$t_ini, t_dep$t_fin, t_dep$event) ~
+      t_dep$Sea_level:strata(tgroup) + t_dep$sinusoid62  +
+      t_dep$sinusoid140:strata(cut_Cox_sin_2[, 11]) +
+      t_dep$`fitness`:strata(cut_Cox_evo[, 11]) +
+      t_dep$SSTdiff + as.factor(t_dep$g_id):strata(cut_Cox_grupos[, 11]) ,
+    data = cut_Cox_f
+  )
+  return(list(cox, model_cuts, cox$coefficients))
+}
+
+
+list_of_coefficients = function(cox, nomb_coef) {
   coef = list()
   for (i in 1:length(nomb_coef)) {
-    a = which(grepl(nomb_coef[i], names(cox[[1]]$coefficients), ignore.case = T) == TRUE)
-    coef[[i]]=cox[[1]]$coefficients[a]
+    a = which(grepl(nomb_coef[i], names(cox[[1]]$coefficients), 
+                    ignore.case = T) == TRUE)
+    coef[[i]] = cox[[1]]$coefficients[a]
   }
-  names(coef)= nomb_coef
+  names(coef) = nomb_coef
   return(coef)
 }
 
 
 base = new_database(sepkoski00)
-model_cuts = list(c(9,26,72), c(), c(6,17,42), c(6,17,42,72),c(),c(5,11,20,30,42))
-names(model_cuts)= c("Sea_level","sinusoid62","sinusoid140", "fitness","SSTdiff","grupos")
+model_cuts = list(c(9, 26, 72),
+                  c(),
+                  c(6, 17, 42),
+                  c(6, 17, 42, 72),
+                  c(),
+                  c(5, 11, 20, 30, 42))
+names(model_cuts) = c("Sea_level",
+                      "sinusoid62",
+                      "sinusoid140",
+                      "fitness",
+                      "SSTdiff",
+                      "grupos")
 
 
-execute_simple_cox = function(base, model_cuts){
-  
+execute_simple_cox = function(base, model_cuts) {
   cox_sin_estrato = simple_cox(base)
   # Proportional hazards test
   temp = cox.zph(cox_sin_estrato)
@@ -124,18 +208,31 @@ execute_simple_cox = function(base, model_cuts){
 
 
 
-execute_cox_complex = function(){
-  cox = cox_complex(base,model_cuts)
-  nomb_coef = c("Sea_level","sinusoid62","sinusoid140", "fitness","SSTdiff","grupos")
-  coeficientes = list_of_coefficients(cox,nomb_coef)
+execute_cox_complex = function() {
+  cox = cox_complex(base, model_cuts)
+  nomb_coef = c("Sea_level",
+                "sinusoid62",
+                "sinusoid140",
+                "fitness",
+                "SSTdiff",
+                "grupos")
+  coeficientes = list_of_coefficients(cox, nomb_coef)
   
   for (i in 1:length(coeficientes)) {
-    j=length(coeficientes[[i]])
-    names(coeficientes[[i]])=rep(names(model_cuts)[i],j)
+    j = length(coeficientes[[i]])
+    names(coeficientes[[i]]) = rep(names(model_cuts)[i], j)
   }
   
   for (i in 1:6) {
-    names(coeficientes[[6]])[(i*7-6):(i*7)]=c("Trilobita","Brachiopoda","Gastropoda","Cephalopoda","Anthozoa","Bivalvia","Otros")
+    names(coeficientes[[6]])[(i * 7 - 6):(i * 7)] = c(
+      "Trilobita",
+      "Brachiopoda",
+      "Gastropoda",
+      "Cephalopoda",
+      "Anthozoa",
+      "Bivalvia",
+      "Otros"
+    )
   }
   
   # Proportional hazards test
@@ -143,9 +240,9 @@ execute_cox_complex = function(){
   
   print(temp_1)
   return(cox)
- # return(list(cox,model_cuts,coeficientes))
+  # return(list(cox, model_cuts, coeficientes))
 }
-c=execute_cox_complex()
+c = execute_cox_complex()
 
 
 
@@ -153,20 +250,34 @@ c=execute_cox_complex()
 #   Residuals analysis                                                      ####
 
 # Deviance
-plot(resid(c[[1]],type="deviance",collapse = base$id),xlab="indice",ylab="residuos(tipo desvio)", main="Residuos (tipo deviance)" )
-summary(resid(c[[1]],type="deviance",collapse = base$id))
+plot(
+  resid(c[[1]], type = "deviance", collapse = base$id),
+  xlab = "indice",
+  ylab = "residuos(tipo desvio)",
+  main = "Residuos (tipo deviance)"
+)
+summary(resid(c[[1]], type = "deviance", collapse = base$id))
 
 # Martingale
-plot(resid(c[[1]],type="martingale",collapse = base$id),xlab="indice",ylab="residuos(tipo martingala)", main="Residuos (tipo martingala)" )
+plot(
+  resid(c[[1]], type = "martingale", collapse = base$id),
+  xlab = "indice",
+  ylab = "residuos(tipo martingala)",
+  main = "Residuos (tipo martingala)"
+)
 
 # Cox-Snell
-di=x$observed
-mt=resid(c[[1]],type="martingale",collapse = base$id)
-cs_res=di-mt
-haz_res=survfit(Surv(cs_res,di)~1,type = "fleming-harrington")
-ch=haz_res$cumhaz
-plot(haz_res$time,ch,xlab = "Cox-Snell residuals", ylab = "Cumulative hazard of residual", main = "Cox-Snell residuals")
-lines(c(0,12),c(0,12))
+di = x$observed
+mt = resid(c[[1]], type = "martingale", collapse = base$id)
+cs_res = di - mt
+haz_res = survfit(Surv(cs_res, di) ~ 1, type = "fleming-harrington")
+ch = haz_res$cumhaz
+plot(haz_res$time,
+     ch,
+     xlab = "Cox-Snell residuals",
+     ylab = "Cumulative hazard of residual",
+     main = "Cox-Snell residuals")
+lines(c(0, 12), c(0, 12))
 
 #saveRDS(model_cuts,"model_cuts.rds")
 #saveRDS(coeficientes,"coeficientes.rds")
